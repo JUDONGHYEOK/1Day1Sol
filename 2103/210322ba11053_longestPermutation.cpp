f244bf6e244811727334a4afb870478fdcd4c6d2
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-03-23 00:34:15 +0900

    210323

--- 2103/210322ba11053_longestPermutation.cpp
+++ 2103/210322ba11053_longestPermutation.cpp
@@ -1,5 +1,36 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
 
 int main(){
-    
+    int number;
+    cin>>number;
+    vector<int> permutation(number+1,0);
+    vector<int> dp(number+1,0);
+
+    for(int i=0;i<number;i++){
+        cin>>permutation[i];
+    }
+    int answer=1;
+    dp[0]=1;
+    for(int i=1;i<number;i++){
+        int check=0;
+        for(int j=0;j<i;j++){
+            if(permutation[i]>permutation[j]){
+                check=max(dp[j],check);
+            }
+        }
+        dp[i]=check+1;
+        for(int k=0;k<i+1;k++){
+            cout<<dp[k]<<" ";
+        }
+        cout<<endl;
+        answer=max(answer,dp[i]);
+    }
+    cout<<answer;
+
+    return 0;
+
 }
