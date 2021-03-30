b680a112f74a5ffa02fbd361c3e9ed21ac66c72b
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-03-30 23:38:16 +0900

    210330

--- 2103/210329ba1912_partitionSum.cpp
+++ 2103/210329ba1912_partitionSum.cpp
@@ -1,8 +1,32 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int main(){
+    int number;
+    cin>>number;
+    vector<int> list(number+1,0);
+    vector<int> dp(number+1,0);
+    for(int i=0;i<number;i++){
+        cin>>list[i];
+    }
+    dp[0]=list[0];
+    for(int i=1;i<number;i++){
+        int check=dp[i-1];
+        int sum=0;
+        for(int j=i;j!=-1;j--){
+            sum+=list[j];
+            dp[i]=max(check,sum);
+        }
+    }
+
+    for(int i=0;i<number;i++){
+        cout<<dp[i]<<" ";
+    }
+    cout<<endl;
+    cout<<dp[number-1];
+    return 0;
     
 }
